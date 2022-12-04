84aff9f29696e8569da5b7ffed8a6cc89e1e860e
williamsiewe16 <loicosiewe16@gmail.com>
2022-12-04 02:00:13 +0100

    adding airflow

---
+++ src/models/train_model.py
@@ -0,0 +1,30 @@
+from sklearn.ensemble import GradientBoostingClassifier
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold
+from pathlib import Path
+
+
+source_path = Path(__file__).resolve()
+data_dir = source_path.parent.parent
+
+train = pd.read_csv(f"{data_dir}/processed/application_train.csv").set_index('SK_ID_CURR')
+test = pd.read_csv(f"{data_dir}/processed/application_train.csv").set_index('SK_ID_CURR')
+
+X_train, X_test = train.iloc[:, 1:245], train.iloc[:, 1:245]
+y_train, y_test = train.TARGET, train.TARGET
+clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0).fit(X_train, y_train)
+clf.get_params()
+
+
+print(classification_report(y_test,clf.predict(X_test)))
+
+clf.score(X_test, y_test)
+
+y_pred = clf.predict(X_test)
+y_pred
+
+
+pd.set_option('display.max_columns', None)
+
+test
