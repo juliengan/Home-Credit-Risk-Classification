a95a78a5b528993102696c1cbf634c7fcc4e40e6
williamsiewe16 <loicosiewe16@gmail.com>
2023-01-24 02:39:04 +0100

    finishing sphinx

--- docs/index.rst
+++ docs/index.rst
@@ -1,18 +1,14 @@
-.. app-BD documentation master file, created by
-   sphinx-quickstart.
+.. Home-Credit-Risk-Classification- documentation master file, created by
+   sphinx-quickstart on Mon Dec  5 16:56:06 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-app-BD documentation!
-==============================================
-
-Contents:
+Welcome to Home-Credit-Risk-Classification-'s documentation!
+============================================================
 
 .. toctree::
    :maxdepth: 2
-
-   getting-started
-   commands
+   :caption: Contents:
 
 
 
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -13,47 +13,47 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore")
 
-
-# get arguments
-n_estimators=int(sys.argv[1])
-learning_rate=int(sys.argv[2])
-max_depth=int(sys.argv[3])
-random_state = int(sys.argv[4])
-tracking_uri = sys.argv[5]
-
-
-source_path = Path(__file__).resolve()
-root_dir = source_path.parent.parent.parent
+if __name__ == '__main__':
+    # get arguments
+    n_estimators=int(sys.argv[1])
+    learning_rate=int(sys.argv[2])
+    max_depth=int(sys.argv[3])
+    random_state = int(sys.argv[4])
+    tracking_uri = sys.argv[5]
 
 
-train = pd.read_csv(f"{root_dir}/data/processed/application_train.csv").set_index('SK_ID_CURR')
-test = pd.read_csv(f"{root_dir}/data/processed/application_train.csv").set_index('SK_ID_CURR')
-
-sample_size=10
-X_train, X_test = train.iloc[:sample_size, 1:245], test.iloc[:sample_size, 1:245]
-y_train, y_test = train.TARGET[:sample_size], test.TARGET[:sample_size]
+    source_path = Path(__file__).resolve()
+    root_dir = source_path.parent.parent.parent
 
 
-#mlflow.set_tracking_uri(tracking_uri)
-mlflow.end_run()
+    train = pd.read_csv(f"{root_dir}/data/processed/application_train.csv").set_index('SK_ID_CURR')
+    test = pd.read_csv(f"{root_dir}/data/processed/application_train.csv").set_index('SK_ID_CURR')
 
-print(mlflow.get_tracking_uri())
+    sample_size=10
+    X_train, X_test = train.iloc[:sample_size, 1:245], test.iloc[:sample_size, 1:245]
+    y_train, y_test = train.TARGET[:sample_size], test.TARGET[:sample_size]
 
-with mlflow.start_run():
-    clf = GradientBoostingClassifier(
-            n_estimators=n_estimators, 
-            learning_rate=learning_rate,
-            max_depth=max_depth, 
-            random_state=random_state
-        ).fit(X_train, y_train)
 
-    clf.get_params()
+    #mlflow.set_tracking_uri(tracking_uri)
+    mlflow.end_run()
 
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("learning_rate", learning_rate)
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_param("random_state", random_state)
+    print(mlflow.get_tracking_uri())
 
-    mlflow.log_metric("score", clf.score(X_test, y_test))
+    with mlflow.start_run():
+        clf = GradientBoostingClassifier(
+                n_estimators=n_estimators, 
+                learning_rate=learning_rate,
+                max_depth=max_depth, 
+                random_state=random_state
+            ).fit(X_train, y_train)
 
-    mlflow.sklearn.log_model(clf, "credit-risk-classifier")
+        clf.get_params()
+
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("learning_rate", learning_rate)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("random_state", random_state)
+
+        mlflow.log_metric("score", clf.score(X_test, y_test))
+
+        mlflow.sklearn.log_model(clf, "credit-risk-classifier")
